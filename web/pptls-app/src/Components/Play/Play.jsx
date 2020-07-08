30e1fd8acf2e6d335f37c60139a486da22ce650c
Emanuel <salgadoema@gmail.com>
2020-07-08 18:31:03 -0300

    Feat: agregue opacida a las imagenes-botnes, agregue nuevas imagenes, nuevos estilos

--- web/pptls-app/src/Components/Play/Play.jsx
+++ web/pptls-app/src/Components/Play/Play.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from 'react-bootstrap';
+import Notifications, {notify} from 'react-notify-toast';
 import '../../Styles/Play.css';
 import piedra from '../../Static/piedra.png'
 import papel from '../../Static/papel.png'
@@ -8,17 +9,18 @@ import lagarto from '../../Static/lagarto.png'
 import spock from '../../Static/spock.png'
 import random from '../../Static/randomSelection.gif'
 import ninguno from '../../Static/ninguno.png'
+import looser from'../../Static/looser.jpg'
 
 function Play() {
 
   const armas = ["piedra", "papel", "tijera", "lagarto", "spock"];
   const [colorBgnd, setColor] = useState({ colorBackgorund: 'violet' });
-  const armasImg = [piedra, papel, tijera, lagarto, spock, random, ninguno]
-  const [unArma, setArma] = useState({ jugadorUno: "5", jugadorDos: "5", ganador: "" })
-
+  const armasImg = [piedra, papel, tijera, lagarto, spock, random, ninguno,looser]
+  const [unArma, setArma] = useState({ jugadorUno: "6", jugadorDos: "5", ganador: "" })
+  let myColor = { background: '#0E1717', text: "#FFFFFF" };
 
   const[statusBtn, setStatusBtn]=useState({habilitado:true});
-
+  const[statusimgLoser, setStatusimgLo]=useState({habilitado:false});
  
   const elegirArmaRandom = () => {
 
@@ -26,11 +28,27 @@ function Play() {
   }
 
 
-
+function deshabilitar() {
+  document.getElementById("myDIV1").style.opacity = "0.5";
+  document.getElementById("myDIV2").style.opacity = "0.5";
+  document.getElementById("myDIV3").style.opacity = "0.5";
+  document.getElementById("myDIV4").style.opacity = "0.5";
+  document.getElementById("myDIV5").style.opacity = "0.5";
+  document.getElementById("myDIV6").style.opacity = "0.5";
+  }
+  function habilitar() {
+    document.getElementById("myDIV1").style.opacity = "1";
+    document.getElementById("myDIV2").style.opacity = "1";
+    document.getElementById("myDIV3").style.opacity = "1";
+    document.getElementById("myDIV4").style.opacity = "1";
+    document.getElementById("myDIV5").style.opacity = "1";
+    document.getElementById("myDIV6").style.opacity = "1";
+    }
  
 
   const elegirArma = (arma) => {
     console.log(statusBtn)
+    deshabilitar();
     if(statusBtn){
     setStatusBtn(false)
     
@@ -46,9 +64,10 @@ function Play() {
   
   }
   const borrarSets = () => {
-    setArma({ jugadorUno: "5", jugadorDos: "5", ganador: "" })
+    setArma({ jugadorUno: "6", jugadorDos: "5", ganador: "" })
     setColor({colorBackgorund:'violet'})
     setStatusBtn({habilitado:true})
+    habilitar()
   }
   /**
   Tijera corta a Papel 
@@ -94,53 +113,48 @@ function Play() {
 
 
         setColor({ colorBackgorund: 'cornflowerblue' });
-        return ("empate");
+        notify.show("EMPATE", "info", 1000, myColor);
+        return ("EMPATE");
         break
       case 1:
         setColor({ colorBackgorund: 'green' });
-        return ("gano Jugador 1");
+        notify.show("GANASTE", "success",1000, myColor);
+        return ("GANASTE");
         break
       case 2:
         setColor({ colorBackgorund: 'red' });
-        return ("gano Jugador 2");
+        notify.show("PERDISTE", "error", 1000, myColor);
+        setStatusimgLo(true);
+        return ("PERDISTE");
         break
     }
   }
+  
 
   return (
     <div className="container" style={{ backgroundColor: colorBgnd.colorBackgorund }} >
+        <Notifications/>
       <div className="batalla">
         <h2 className="jug1">Jugador uno:{}</h2>
-        <img className="imgBatalla" src={armasImg[unArma.jugadorUno]}></img>
-        <h1>VS</h1>
+        <img  className="imgBatalla" src={armasImg[unArma.jugadorUno]} ></img>
+        <h1>VS</h1> 
         <img className="imgBatalla" src={armasImg[unArma.jugadorDos]}></img>
         <h2 className="jug2">Jugador dos{armasImg.jugadorDos}</h2>
       </div>
       <div className="containerBtn">
        
-          <h2>Elegi tu arma</h2>
-      <div className="responsive">
-
-        <img className="imgArma" src={armasImg[0]} onClick={() => elegirArma(0)}></img>
         
-       
-        <img className="imgArma" src={armasImg[1]} onClick={() => elegirArma(1)} ></img>
-
-       
-        <img className="imgArma" src={armasImg[2]} onClick={() => elegirArma(2)} ></img>
-
-         
-        <img className="imgArma" src={armasImg[3]} onClick={() => elegirArma(3)} ></img>
-
-       
-        <img className="imgArma" src={armasImg[4]} onClick={() => elegirArma(4)} ></img>
-
+      <div className="responsive">
+      <h2>Elegi tu arma</h2>
       
-        <img className="imgArma" src={armasImg[5]} onClick={() => elegirArma(elegirArmaRandom())} ></img>
-
-          <Button className="btnWeapon" onClick={() => borrarSets()} variant="primary">Reiniciar</Button>
-
-        </div>
+        <img id="myDIV1" className="imgArma" src={armasImg[0]} onClick={() => elegirArma(0)}  ></img>
+        <img id="myDIV2" className="imgArma" src={armasImg[1]} onClick={() => elegirArma(1)} ></img>
+        <img id="myDIV3" className="imgArma" src={armasImg[2]} onClick={() => elegirArma(2)} ></img>
+        <img id="myDIV4" className="imgArma" src={armasImg[3]} onClick={() => elegirArma(3)} ></img>
+        <img id="myDIV5" className="imgArma" src={armasImg[4]} onClick={() => elegirArma(4)} ></img>
+        <img id="myDIV6" className="imgArma" src={armasImg[5]} onClick={() => elegirArma(elegirArmaRandom())} ></img>
+        <Button className="btnWeapon" onClick={() => borrarSets()} variant="primary">Reiniciar</Button>
+      </div>
   
         <h1>{unArma.ganador}</h1>
       </div>
