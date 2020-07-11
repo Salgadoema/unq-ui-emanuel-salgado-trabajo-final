9b2375488ef752ec3a986dce9a454fedb5c971b7
Emanuel <salgadoema@gmail.com>
2020-07-11 16:06:57 -0300

    cambios poco significantes

--- web/pptls-app/src/App.js
+++ web/pptls-app/src/App.js
@@ -1,6 +1,5 @@
 
 import React, { useState } from "react";
-import logo from './logo.svg';
 import './App.css';
 import { useHistory} from 'react-router-dom';
 import piedra from './Static/piedra.png'
@@ -8,7 +7,6 @@ import papel from './Static/papel.png'
 import tijera from './Static/tijera.png'
 import lagarto from './Static/lagarto.png'
 import spock from './Static/spock.png'
-import {Button } from 'react-bootstrap';
 import Notifications, { notify } from 'react-notify-toast';
 function App() {
   let history = useHistory();
@@ -35,19 +33,19 @@ function App() {
         <Notifications />
             <div className="row">
             <div className="column">
-                 <h1 className="title2"> P </h1>
+                 <h1 className="title2"> Pi </h1>
                  <img className="App-logo" src={piedra}/>
             </div>
             <div class="column">
-                 <h1 className="title2"> P </h1>
+                 <h1 className="title2"> Pa </h1>
                  <img className="App-logo"src={papel}/>
             </div>
             <div class="column">
-                 <h1 className="title2"> T </h1>
+                 <h1 className="title2"> Ti </h1>
                  <img className="App-logo"src={tijera} />
             </div>
             <div class="column">
-                 <h1 className="title2"> L </h1>
+                 <h1 className="title2"> La </h1>
                  <img className="App-logo" src={lagarto} />
             </div>
             <div class="column">
@@ -57,13 +55,13 @@ function App() {
           </div>
 
           <div class="column">
-         <div>BIENVENIDO AL JUEGO PIEDRA-PAPEL-TIJERA-LAGARTO-SPOCK</div>
+         <div>BIENVENIDO AL JUEGO PiPaTiLaS...</div>
          <div >INGRESA TU NOMBRE: 
          <input className="inp" type="text" placeholder=" Username" name="uname"
                   onChange={updateName}
                   required/>
                   
-                   <button className="inp1" type="button" onClick={()=>toPlay()}  >A JUGAR!!!</button>
+                   <button className="inp1" type="button" onClick={()=>toPlay()}  >1 Jugador</button>
                    <button className="inp2" type="button" onClick={()=>toRules()}  >REGLAS</button>
                    
         </div> 
