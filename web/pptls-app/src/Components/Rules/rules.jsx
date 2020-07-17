8b51bddac292b146df52206abaea28f8f972c867
Emanuel <salgadoema@gmail.com>
2020-07-17 10:01:17 -0300

    refactor codigo

--- web/pptls-app/src/Components/Rules/rules.jsx
+++ web/pptls-app/src/Components/Rules/rules.jsx
@@ -13,7 +13,10 @@ function Rules () {
             
             <button className="btnToHome" type="button" onClick={iraHome}>Back to Home</button>
             <div className="contenedor2">
-            <iframe title="reproductor" width="1200" height="800" src={"https://www.youtube.com/embed/3FAnlDUDbLA?autoplay=1&enablejsapi=1"} frameborder="0" allowfullscreen/>
+                <iframe title="reproductor" width="1200" height="800" 
+                    src={"https://www.youtube.com/embed/3FAnlDUDbLA?autoplay=1&enablejsapi=1"} 
+                    frameBorder="0" 
+                    allowFullScreen/>
             </div>
         </div>
     );
